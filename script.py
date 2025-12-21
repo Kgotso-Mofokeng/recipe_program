151753c8fb26f1447a7410ce46ecd028ee42bef3
Kgotso-Mofokeng <156092043+Kgotso-Mofokeng@users.noreply.github.com>
2025-12-21 21:25:34 +0200

    Program version two

--- script.py
+++ script.py
@@ -1,21 +1,60 @@
 # Simple Recipe Cost Calculator
 
-
 print("RECIPE COST CALCULATOR")
 print("----------------------")
 
-recipe_name = input("Enter recipe name: ")
-total_cost = 0.0
+# Data Structures , recipes should be empty, but is populated for test purposes
+recipes = {
+    "Green Pasta":{
+        "Spaghetti": 30,
+        "Red cabbage": 35,
+        "Regular cabbage": 25,
+        "Onions": 20,
+        "Ground beef (mince)": 60,
+        "Carrots": 25,
+        "Potatoes": 15,
+        "Salt": 35,
+        "Barbeque spice": 50,
+        "Sunflower oil": 25
+    },
+
+    "Mince Macaroni":{
+        "Macaroni": 67,
+        "Beef mincemeat (lean)": 35,
+        "Tomato paste": 35,
+        "Braai sauce": 37,
+        "Onion": 13,
+        "Green pepper": 30,
+        "Garlic (crushed)": 56,
+        "Mild Rajah": 76,
+        "Beef spice": 45,
+        "Beef stock cubes": 2,
+        "Sunflower oil": 34
+    }
+}
+
+
 
 while True:
-    ingredient = input("Enter ingredient name (or type 'done'): ")
-    if ingredient.lower() == "done":
+    recipe_name = input("\nEnter recipe name (or 'exit'): ")
+    if recipe_name.lower() == "exit":
         break
 
-    price = float(input("Enter price of {ingredient}: ".format(ingredient=ingredient)))
-    total_cost = total_cost + price
+    recipes[recipe_name] = {}
 
-print("\n---------------------")
-print("Recipe:", recipe_name)
-print("Total cost: R", round(total_cost, 2))
-print("-----------------------")
+    while True:
+        ingredient = input("Enter ingredient name (or type 'done'): ")
+        if ingredient.lower() == "done":
+            break
+
+        price = float(input("Enter price of {ingredient}: ".format(ingredient=ingredient)))
+        recipes[recipe_name][ingredient] = price
+        
+
+
+
+def compare_recipes(recipes_dict):
+    for recipe, ingredients in recipes.items():
+        total = sum(ingredients.values())
+        print(recipe, "-> R", round(total, 2))
+
